d163bb3c8aef78cdcb34d4406b545a71bef710a5
carol <lu773414@gmail.com>
2023-04-28 16:56:18 +0800

    Route 路徑改變

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/" element={<County />} />
+          <Route path="/react_d3js_practise" element={<County />} />
         </Routes>
       </Router>
     </>
